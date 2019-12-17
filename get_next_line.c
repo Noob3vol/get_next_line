38e9bae3f7eb8aa725d814ba0da806c139c9175f
Noob 3vol <noob.3vol@gmail.com>
2019-12-17 01:25:36 +0100

    New shot : feature memory cleanness

--- get_next_line.c
+++ get_next_line.c
@@ -1,48 +1,178 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   gnl.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: iguidado <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/30 03:30:31 by iguidado          #+#    #+#             */
-/*   Updated: 2019/12/06 01:03:48 by iguidado         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "get_next_line.h"
 #include <unistd.h>
 #include <stdlib.h>
 
-int	ft_handle_error(int fd, char **line)
+void	ft_manage_buf(char *buf)
 {
-	if (fd < 0)
-		return (1);
-	if (!line)
-		return (1);
-	*line = NULL;
-	return (0);
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (buf[i] && buf[i] != '\n' && i < BUFFER_SIZE)
+		i++;
+	while (++i < BUFFER_SIZE)
+	{
+		buf[j] = buf[i];
+		j++;
+	}
+	while (j < BUFFER_SIZE)
+	{
+		buf[j] = 0;
+		j++;
+	}
 }
 
+int	ft_dump_line(char *buf, char **line)
+{
+	int i;
+	int j;
+	int ret;
+	char *new;
+
+	if (!*buf)
+		return (0);
+	i = 0;
+	j = 0;
+	ret = 0;
+	while (buf[i] && buf[i] != '\n' && i < BUFFER_SIZE)
+		i++;
+	while ((*line) && (*line)[j])
+		j++;
+	if (buf[i] == '\n')
+		ret = 1;
+	new = (char *)malloc(sizeof(char) * (i + j + 1));
+	new[i + j] = 0;
+	while (i-- > 0)
+		new[j + i] = buf[i];
+	while (j-- > 0)
+		new[j] = (*line)[j];
+	if (*line)
+		free(*line);
+	*line = new;
+	ft_manage_buf(buf);
+	return (ret);
+}
+	
 int	get_next_line(int fd, char **line)
 {
-	static char buffer[BUFFER_SIZE] = "";
-	int			ret;
+	static char	buff[BUFFER_SIZE + 1];
+	int		ret;
 
-	if(ft_handle_error(fd, line))
+	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
-	while (ft_dump_buf(buffer, line) == 1)
+	*line = NULL;
+	ret = 0;
+	while (!ft_dump_line(buff, line))
 	{
-		if ((ret = read(fd, buffer, BUFFER_SIZE)) <= 0)
+		if ((ret = read(fd, buff, BUFFER_SIZE)) < 1)
 		{
-			if (!ret)
+			if (ret == -1)
 			{
 				if (*line)
 					free(*line);
-				*line = NULL;
+				line = NULL;
 			}
 			return (ret);
 		}
 	}
 	return (1);
 }
+
+
+/*
+**	#include <fcntl.h>
+**	#include <stdio.h>
+**	#include "libft.h"
+**
+**	int	unit_test_ft_arr_buf(void)
+**	int	main(void)
+**	{
+**		char sample[BUFFER_SIZE] = "Hello\nWorl";
+**	
+**		ft_arr_buf(sample);
+**		ft_print_memory((void *)sample, sizeof(sample));
+**		return (0);
+**	}
+*/
+
+/*
+** La compilation se fait sans DFLAG car il y aura toujours des leaks lié
+** à la reutilisation de la variable *line pour stocké la ligne
+** donc un leak de *line à chaque appel
+*/
+/*
+**	int	strequ(char *str, char *ref)
+**	{
+**		int i;
+**	
+**		i = 0;
+**		while (str[i])
+**		{
+**			if (str[i] != ref[i])
+**				return (0);
+**			i++;
+**		}
+**		return (1);
+**	}
+**	
+**	int	test_read_1_file(int (*gnl)(int, char **), int fd)
+**	{
+**		char *line = NULL;
+**		int ret = 1;
+**		int count = 0;
+**		char ref[] = "a";
+**	
+**		while (count < 25)
+**		{
+**			ret = gnl(fd, &line);
+**			printf("-[%d]-\t%s\n", ret, line);
+**			if (!strequ(line, ref))
+**				return (printf("Bad line Assignement"));
+**			if (ret != 1)
+**				return (printf("Mauvais retour de gnl"));
+**			ref[0] += 1;
+**			count++;
+**		}
+**		ret = gnl(fd, &line);
+**		printf("-[%d]-\t%s\n", ret, line);
+**		if (!strequ(line, ref))
+**			return (printf("Bad line Assignement"));
+**		if (ret != 0)
+**			return (printf("Mauvais retour de gnl"));
+**		return (0);
+**	}
+**	
+**	int	test_exception(int (*gnl)(int, char **), int fd)
+**	{
+**		int ret;
+**		char *line = NULL;
+**	
+**		ret = gnl(-30, &line);
+**		if (line)
+**			return(printf("Line Assignement on ERROR\n"));
+**		if (ret != -1)
+**			return (printf("Bad Return on ERROR\n%d instead of -1\n", ret));
+**		ret = gnl(fd, NULL);
+**		if (line)
+**			return(printf("Line Assignement on ERROR\n"));
+**		if (ret != -1)
+**			return (printf("Bad Return on ERROR\n%d instead of -1\n", ret));
+**		return (0);
+**	}
+**	
+**	int	main(void)
+**	{
+**		int fd;
+**		char	*line = NULL;
+**	
+**		fd = open("test.txt", O_RDONLY, 0);
+**		test_exception(get_next_line, fd);
+**		fd = open("test.txt", O_RDONLY, 0);
+**		test_read_1_file(get_next_line, fd);
+**		printf("-[%d]-\t%s", get_next_line(fd, &line), line);
+**		//	ft_putendl("BUFFER_SIZE ERROR non handled");
+**	//	fd = open("test.txt", O_RDONLY, 0);
+**	//	test_read_2_file(get_next_line, fd);
+**		return (0);
+**	}
+*/
